6d847766e37b1ebde6cdc551fc703a722bb3895f
Scott <scott.brown4@nike.com>
2020-03-30 09:39:47 -0700

    Adding serverless API

---
+++ create.js
@@ -0,0 +1,26 @@
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+
+import { failure, success } from "./libs/response-lib";
+
+import uuid from "uuid";
+
+export async function main(event, context) {
+  const data = JSON.parse(event.body);
+  const params = {
+    TableName: process.env.tableName,
+    Item: {
+      userId: event.requestContext.identity.cognitoIdentityId,
+      noteId: uuid.v1(),
+      content: data.content,
+      attachment: data.attachment,
+      createdAt: Date.now()
+    }
+  };
+
+  try {
+    await dynamoDbLib.call("put", params);
+    return success(params.Item);
+  } catch (e) {
+    return failure({ status: false });
+  }
+}
